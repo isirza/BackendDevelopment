479c82f643b8a44ea9a4ce4297415ec91d3ed89a
İsmayil Rzayev <isirza21@gmail.com>
2024-02-05 12:46:25 +0400

    lesson-3

---
+++ lesson-3/calculator/calculator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("1 ci ededidaxil edin ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2ci ededi daxil edin");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("simvolu daxil edin  + - * / ");
+            char sign = Convert.ToChar(Console.ReadLine());
+
+
+            if(sign=='+')
+            {
+                Console.WriteLine(x1 + x2);
+
+            }else if(sign=='-')
+            {
+                Console.WriteLine(x1 - x2);
+
+            }
+            else if (sign=='*')
+
+            {
+                Console.WriteLine(x1 * x2);
+
+
+            }
+            else if (sign =='/')
+            {
+                Console.WriteLine(x1 / x2);
+
+            }
+            else
+
+
+            {
+                Console.WriteLine("bele bir isare movcud deyildir");
+            }
+        }
+    }
+}
---
+++ lesson-3/switch/switch/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1ci ededi daxil edin");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2ci ededi daxil edin");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("simvolu  daxil edin + - * /");
+            char sign = Convert.ToChar(Console.ReadLine());
+           
+            switch (sign)
+            {
+                            
+            case '+':
+            Console.WriteLine($"{x1} {sign} {x2} = {x1 + x2}");
+            break;
+
+
+
+            case '-':
+            Console.WriteLine($"{x1} {sign} {x2} = {x1 - x2}");
+            break;
+
+
+
+            case '*':
+                    Console.WriteLine($"{x1} {sign} {x2} = {x1 * x2}");
+            break;
+
+
+
+            case '/':
+                    Console.WriteLine($"{x1} {sign} {x2} = {x1 / x2}");
+                        
+            break;
+
+
+
+
+
+            }
+       
+
+
+        }
+    }
+}
